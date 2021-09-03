58fcdb8e36427fdf5d3c85ad671afd3cae0f4cea
Weckyy702 <weckyy702@gmail.com>
2021-09-03 11:15:37 +0200

    Finished implementing ArgumentParser

--- include/ArgumentParser.h
+++ include/ArgumentParser.h
@@ -28,115 +28,264 @@
 #ifndef RAYCHEL_CORE_ARGUMENT_PARSER_H
 #define RAYCHEL_CORE_ARGUMENT_PARSER_H
 
+#include <charconv>
 #include <map>
+#include <optional>
 #include <string>
 #include <string_view>
-#include <charconv>
 #include "CommandLineValueReference.h"
 #include "RaychelLogger/Logger.h"
 #include "Raychel_assert.h"
 
 namespace Raychel {
 
-    bool case_insensitive_equal(std::string_view lhs, std::string_view rhs) noexcept
+    inline bool case_insensitive_equal(std::string_view lhs, std::string_view rhs) noexcept
     {
-        if(lhs.size() != rhs.size()) {
+        if (lhs.size() != rhs.size()) {
             return false;
         }
-        if(lhs.data() == rhs.data()) {
+        if (lhs.data() == rhs.data()) {
             return true;
         }
-        
-        for(std::size_t i = 0; i < lhs.size(); i++) {
-            if(std::tolower(lhs[i]) != std::tolower(rhs[i])) {
-                return false;   
+
+        for (std::size_t i = 0; i < lhs.size(); i++) {
+            if (std::tolower(lhs[i]) != std::tolower(rhs[i])) {
+                return false;
             }
         }
 
         return true;
     }
 
+    struct CommandLineKey
+    {
+        std::string long_name;
+        std::string short_name;
+        std::string description;
+
+        auto operator<=>(const CommandLineKey&) const noexcept=default;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const CommandLineKey& obj)
+    {
+        return os << "{ '" << obj.long_name << "', '" << obj.short_name << "' }";
+    }
+
     class ArgumentParser
     {
+
+        using ArgumentMap_t = std::map<CommandLineKey, CommandLineValueReference>;
+
     public:
         ArgumentParser() = default;
 
-        bool add_bool_arg(std::string_view name, bool& value_ref) noexcept
+        /**
+        * \brief add an argument of type int to the parser
+        * 
+        * \param name name for the argument. Will match --name
+        * \param short_name short name for the argument. Will match -short_name
+        * \param description description for the argument. Used when printing Usage
+        * \param value_ref reference for the argument. Will not be mutated if parsing fails
+        * \return wether the argument could be added succesfully (arguments must be unique)
+        */
+        bool
+        add_int_arg(std::string_view name, std::string_view short_name, std::string_view description, int& value_ref) noexcept
         {
-            return _add_arg(name, value_ref);
+            return _add_arg(name, short_name, description, value_ref);
         }
 
-        bool add_int_arg(std::string_view name, int& value_ref) noexcept
+        /**
+        * \brief add an argument of type float to the parser
+        * 
+        * \param name name for the argument. Will match --name
+        * \param short_name short name for the argument. Will match -short_name
+        * \param description description for the argument. Used when printing Usage
+        * \param value_ref reference for the argument. Will not be mutated if parsing fails
+        * \return wether the argument could be added succesfully (arguments must be unique)
+        */
+        bool
+        add_float_arg(std::string_view name, std::string_view short_name, std::string_view description, float& value_ref) noexcept
         {
-            return _add_arg(name, value_ref);
+            return _add_arg(name, short_name, description, value_ref);
         }
 
-        bool add_float_arg(std::string_view name, float& value_ref) noexcept
+        /**
+        * \brief add an argument of type string to the parser
+        * 
+        * \param name name for the argument. Will match --name
+        * \param short_name short name for the argument. Will match -short_name
+        * \param description description for the argument. Used when printing Usage
+        * \param value_ref reference for the argument. Will not be mutated if parsing fails
+        * \return wether the argument could be added succesfully (arguments must be unique)
+        */
+        bool add_string_arg(
+            std::string_view name, std::string_view short_name, std::string_view description, std::string& value_ref) noexcept
         {
-            return _add_arg(name, value_ref);
+            return _add_arg(name, short_name, description, value_ref);
         }
 
-        bool add_string_arg(std::string_view name, std::string& value_ref) noexcept
-        {
-            return _add_arg(name, value_ref);
-        }
 
-
-
+        /**
+        * \brief Parse the command line arguments and print usage string if parsing fails
+        * 
+        * \param argc argc that was given to main()
+        * \param argv argv that was given to main()
+        * \return wether parsing was successful (most programs exit if parsing fails)
+        */
         [[nodiscard]] bool parse(int argc, char** argv) noexcept
         {
-            //TODO: clean this up and implement error handling [ have fun ;) ]
-            using ValueType = CommandLineValueReference::ValueType;
-            (void)this;
-            bool should_exit = false;
+            //-h or --help should not mutate any state and exit immediately
+            if (handle_help_arg(argc, argv)) {
+                return false;
+            }
 
-            for(int i = 0; i < argc-1; i++) { //the last command line argument cannot be a key
-                std::string_view current_arg = argv[i];
+            for (int i = 1; i < argc - 1; i += 2) {      //the first and last command line argument cannot be keys
+                std::string_view current_arg = argv[i];  //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+                std::string_view next_arg = argv[i + 1]; //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
-                if(!current_arg.starts_with("--")) {
-                    continue;
+                if (!current_arg.starts_with('-')) {
+                    Logger::error("Argument '", current_arg, "' is incorrectly formatted!\n");
+                    goto parse_failed; //NOLINT(hicpp-avoid-goto, cppcoreguidelines-avoid-goto): Code duplication is worse than goto
                 }
-                current_arg.remove_prefix(2U);
-
-                for(const auto&[key, value] : arguments_) {
-                    if(key == current_arg) {
-                        Logger::debug("Found key ", key, '\n');
-                        std::string_view value_str = argv[i+1];
-                        switch(value.type()) {
-                            case ValueType::bool_:
-                                value.as_bool() = case_insensitive_equal(value_str, "true");
-                                break;
-                            case ValueType::int_:
-                                value.as_int() = std::atoi(value_str.data());
-                                break;
-                            case ValueType::float_:
-                                std::from_chars(std::begin(value_str), std::end(value_str), value.as_float());
-                                break;
-                            case ValueType::string_:
-                                value.as_string() = value_str;
-                                break;
-                        }
-                        break;
+                if (current_arg.starts_with("--")) {
+                    //parse argument in the form --key value
+                    current_arg.remove_prefix(2U);
+                    if (!_parse_long_arg(current_arg, next_arg)) {
+                        goto parse_failed; //NOLINT(hicpp-avoid-goto, cppcoreguidelines-avoid-goto): Code duplication is worse than goto
+                    }
+                } else {
+                    //parse argument in the form -key value
+                    current_arg.remove_prefix(1U);
+                    if (!_parse_short_arg(current_arg, next_arg)) {
+                        goto parse_failed; //NOLINT(hicpp-avoid-goto, cppcoreguidelines-avoid-goto): Code duplication is worse than goto
                     }
                 }
             }
+            return true;
 
-            return !should_exit;
+        parse_failed:
+            _show_usage();
+            return false;
         }
 
     private:
         template <typename T>
-        [[nodiscard]] bool _add_arg(std::string_view name_, T& value_ref) noexcept
+        [[nodiscard]] bool
+        _add_arg(std::string_view name_, std::string_view short_name_, std::string_view description_, T& value_ref) noexcept
         {
-            const auto name = std::string{name_};
-            if (arguments_.contains(name)) {
+            const CommandLineKey key{std::string{name_}, std::string{short_name_}, std::string{description_}};
+            if (arguments_.contains(key)) {
                 return false;
             }
 
-            return arguments_.insert({name, CommandLineValueReference{value_ref}}).second;
+            return arguments_.insert({key, CommandLineValueReference{value_ref}}).second;
         }
 
-        std::map<std::string, CommandLineValueReference> arguments_;
+
+
+        [[nodiscard]] bool _parse_long_arg(std::string_view current_arg, std::string_view value_str) noexcept
+        {
+            const auto pair_opt = _find_value_reference_for_key(current_arg, true);
+            if (!pair_opt) {
+                Logger::error("Unknown option '", current_arg, "'!\n");
+                return false;
+            }
+            return _populate_value_ref(value_str, pair_opt->second);
+        };
+
+        [[nodiscard]] bool _parse_short_arg(std::string_view current_arg, std::string_view value_str) noexcept
+        {
+            const auto pair_opt = _find_value_reference_for_key(current_arg, false);
+            if (!pair_opt) {
+                Logger::error("Unknown option '", current_arg, "'!\n");
+                return false;
+            }
+            return _populate_value_ref(value_str, pair_opt->second);
+        }
+
+
+
+        [[nodiscard]] std::optional<ArgumentMap_t::value_type>
+        _find_value_reference_for_key(std::string_view arg_key, bool long_arg) const noexcept
+        {
+            const auto _compare_key = [&arg_key, long_arg](const CommandLineKey& key) {
+                if (long_arg) {
+                    return key.long_name == arg_key;
+                }
+            return key.short_name == arg_key;
+            };
+
+            for (const auto& pair : arguments_) {
+                if (_compare_key(pair.first)) {
+                    return pair;
+                }
+            }
+            return std::nullopt;
+        }
+
+
+
+        [[nodiscard]] static bool _populate_value_ref(std::string_view value_str, const CommandLineValueReference& value) noexcept
+        {
+            using Type = CommandLineValueReference::ValueType;
+
+            switch (value.type()) {
+                case Type::int_:
+                    return _parse_int(value_str, value);
+                case Type::float_:
+                    return _parse_float(value_str, value);
+                case Type::string_:
+                    value.as_string() = value_str;
+                    return true;
+            }
+            RAYCHEL_ASSERT_NOT_REACHED;
+        }
+
+        [[nodiscard]] static bool _parse_int(std::string_view value_str, const CommandLineValueReference& value) noexcept
+        {
+            const auto res = std::from_chars(std::begin(value_str), std::end(value_str), value.as_int());
+
+            if (res.ec != std::errc{}) {
+                Logger::error("Could not parse value '", value_str, "' as an int!\n");
+                return false;
+            }
+            return true;
+        }
+
+        [[nodiscard]] static bool _parse_float(std::string_view value_str, const CommandLineValueReference& value) noexcept
+        {
+            const auto res = std::from_chars(std::begin(value_str), std::end(value_str), value.as_float());
+
+            if (res.ec != std::errc{}) {
+                Logger::error("Could not parse value '", value_str, "' as a float!\n");
+                return false;
+            }
+            return true;
+        }
+
+
+
+        [[nodiscard]] bool handle_help_arg(int argc, char** argv) noexcept
+        {
+            for (int i = 0; i < argc; i++) {
+                std::string_view arg = argv[i]; //NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+                if (arg == "-h" || arg == "--help") {
+                    _show_usage();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void _show_usage() noexcept
+        {
+            Logger::log("USAGE:\n\t--help -h\tsee this message and exit\n");
+            for (const auto& [key, _] : arguments_) {
+                Logger::log("\t--", key.long_name, " -", key.short_name, '\t', key.description, '\n');
+            }
+        }
+
+        ArgumentMap_t arguments_;
     };
 
 } // namespace Raychel
--- test/ArgumentParser.cpp
+++ test/ArgumentParser.cpp
@@ -2,8 +2,6 @@
 #include <cstdlib>
 
 static struct State{
-
-    bool b{false};
     int i{0};
     float f{0};
     std::string str;
@@ -12,18 +10,16 @@ static struct State{
 
 int main(int argc, char** argv)
 {
+    Logger::setMinimumLogLevel(Logger::LogLevel::debug);
     Raychel::ArgumentParser ap;
 
-    ap.add_bool_arg("bool_arg", state.b);
-    ap.add_int_arg("int_arg", state.i);
-    ap.add_float_arg("float_arg", state.f);
-    ap.add_string_arg("string_arg", state.str);
+    ap.add_int_arg("iterations", "n", "number of iterations", state.i);
 
     if(!ap.parse(argc, argv)) {
         return EXIT_FAILURE;
     }
 
-    Logger::log("STATE: ", state.b, " : ", state.i, " : ", state.f, " : ", state.str, '\n');
+    Logger::log("STATE: ", state.i, " : ", state.f, " : ", state.str, '\n');
 
     return EXIT_SUCCESS;
 }
