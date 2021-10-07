8b927f0cccb4d57cda77cbb2f3638b034aeafe19
Weckyy702 <weckyy702@gmail.com>
2021-10-07 16:43:43 +0200

    Added asserting wrappers around std::get

---
+++ include/AssertingGet.h
@@ -0,0 +1,52 @@
+/*DONT FORGET THE COPYRIGHT ;)*/
+#ifndef RAYCHEL_ASSERTING_GET_H
+#define RAYCHEL_ASSERTING_GET_H
+
+#include <variant>
+#include "Raychel_assert.h"
+
+namespace Raychel {
+
+    template <typename T, typename... Ts>
+    constexpr T& get(std::variant<Ts...>& v) noexcept
+    {
+        try {
+            return std::get<T>(v);
+        } catch (std::bad_variant_access&) {
+            RAYCHEL_ASSERT_NOT_REACHED;
+        }
+    }
+
+    template <typename T, typename... Ts>
+    constexpr T&& get( std::variant<Ts...>&& v ) noexcept
+    {
+        try {
+            return std::get<T>(std::forward<std::variant<Ts...>&&>(v));
+        } catch (std::bad_variant_access&) {
+            RAYCHEL_ASSERT_NOT_REACHED;
+        }
+    }
+
+    template <typename T, typename... Ts>
+    constexpr const T& get(const std::variant<Ts...>& v) noexcept
+    {
+        try {
+            return std::get<T>(v);
+        } catch (std::bad_variant_access&) {
+            RAYCHEL_ASSERT_NOT_REACHED;
+        }
+    }
+
+    template <typename T, typename... Ts>
+    constexpr const T&& get(const std::variant<Ts...>&& v) noexcept
+    {
+        try {
+            return std::get<T>(std::forward<std::variant<Ts...>&&>(v));
+        } catch (std::bad_variant_access&) {
+            RAYCHEL_ASSERT_NOT_REACHED;
+        }
+    }
+
+} // namespace Raychel
+
+#endif //!RAYCHEL_ASSERTING_GET_H
---
+++ test/AssertingGet.test.cpp
@@ -0,0 +1,31 @@
+#include "AssertingGet.h"
+#include <catch2/catch.hpp>
+
+//TODO: extend this test suite once Catch2 allows checking for std::terminate
+
+TEST_CASE("AssertingGet", "[RaychelCore][Utilities]")
+{
+    std::variant<int, double> v{1.0};
+    const auto d = Raychel::get<double>(v);
+    REQUIRE(d == 1.0);
+}
+
+TEST_CASE("const AssertingGet", "[RaychelCore][Utilities]")
+{
+    const std::variant<int, double> v{1.0};
+    const auto d = Raychel::get<double>(v);
+    REQUIRE(d == 1.0);
+}
+
+TEST_CASE("rvalue AssertingGet", "[RaychelCore][Utilities]")
+{
+    const auto d = Raychel::get<double>(std::variant<int, double>{1.0});
+    REQUIRE(d == 1.0);
+}
+
+TEST_CASE("constexpr AssertingGet", "[RaychelCore][Utilities]")
+{
+    constexpr std::variant<int, double> v{1.0};
+    constexpr auto d = Raychel::get<double>(v);
+    REQUIRE(d == 1.0);
+}
