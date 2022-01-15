b122957b3da6fc956b535fb8a832582ffdead14c
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-01-15 14:51:05 +0100

    [Append] disable unit tests by default

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -1,21 +1,23 @@
 include(FetchContent)
 
-find_package(Catch2 QUIET)
-
-if(NOT Catch2_FOUND)
-
-    message(STATUS "Could not find a local installation of Catch2, downloading one off github...")
-
-    FetchContent_Declare(CATCH_2
-        GIT_REPOSITORY "https://github.com/catchorg/Catch2"
-        GIT_TAG "v2.13.6"
-    )
-
-    FetchContent_MakeAvailable(CATCH_2)
-
-    set(CATCH_2_EXTERNAL true)
-    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${Catch2_SOURCE_DIR}/contrib)
-
+if(RAYCHELCORE_BUILD_TESTS)
+    find_package(Catch2 QUIET)
+    
+    if(NOT Catch2_FOUND)
+    
+        message(STATUS "Could not find a local installation of Catch2, downloading one off github...")
+    
+        FetchContent_Declare(CATCH_2
+            GIT_REPOSITORY "https://github.com/catchorg/Catch2"
+            GIT_TAG "v2.13.6"
+        )
+    
+        FetchContent_MakeAvailable(CATCH_2)
+    
+        set(CATCH_2_EXTERNAL true)
+        set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${Catch2_SOURCE_DIR}/contrib)
+    
+    endif()
 endif()
 
 find_package(RaychelLogger QUIET)
