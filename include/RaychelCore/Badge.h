3391bdb46ef4d06d629a59c1c784212acb176e16
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-04-12 22:50:26 +0200

    Badge: fix friend declaration shadowing template parameter

--- include/RaychelCore/Badge.h
+++ include/RaychelCore/Badge.h
@@ -3,7 +3,7 @@
 * \author Weckyy702 (weckyy702@gmail.com)
 * \brief Header file for Badge class
 * \date 2022-02-19
-* 
+*
 * MIT License
 * Copyright (c) [2022] [Weckyy702 (weckyy702@gmail.com | https://github.com/Weckyy702)]
 * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -12,10 +12,10 @@
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,18 +23,18 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
-* 
+*
 */
 #ifndef RAYCHELCORE_BADGE_H
 #define RAYCHELCORE_BADGE_H
 
-namespace RaychelCore {
+namespace Raychel {
 
     template<typename T>
     class Badge {
         private:
             Badge()=default;
-            friend class T;
+            friend T;
     };
 
 }//namespace RaychelCore
