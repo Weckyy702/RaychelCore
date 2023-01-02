031bc6badaae138c17350dc776e8bdd690712439
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2023-01-02 16:47:45 +0100

    SmallBuffer: Fix move operations and provide an API to access the underlying storage

--- include/RaychelCore/SmallBuffer.h
+++ include/RaychelCore/SmallBuffer.h
@@ -32,6 +32,7 @@
 #include "Raychel_assert.h"
 
 #include <cstddef>
+#include <cstring>
 #include <memory>
 #include <new>
 #include <type_traits>
@@ -50,52 +51,116 @@ namespace Raychel {
                 if (do_delete)
                     operator delete(ptr);
             }
+
+            static void do_copy(const void* _src, void* dst) noexcept
+            {
+                const auto* src = std::launder(reinterpret_cast<const T*>(_src));
+
+                new (dst) T{*src};
+            }
         };
     } // namespace details
 
     template <std::size_t BufferSize>
     class SmallBuffer
     {
-        using DestroyingFunction = void (*)(void*, bool) noexcept;
+        using DestroyFunction = void (*)(void*, bool) noexcept;
+        using CopyFunction = void (*)(const void*, void*) noexcept;
 
     public:
         SmallBuffer() = default;
 
         template <typename T>
-        explicit SmallBuffer(T&& value)
+        requires(!std::is_same_v<std::remove_cvref_t<T>, SmallBuffer>) explicit SmallBuffer(T&& value)
         {
             emplace(std::forward<T>(value));
         }
 
-        template <typename T>
-        T& emplace(T&& value)
+        SmallBuffer(SmallBuffer&& other) noexcept
+            : do_destroy_{other.do_destroy_}, do_copy_{other.do_copy_}, object_size_(other.object_size_)
         {
+            _move(std::move(other));
+        }
+
+        SmallBuffer(const SmallBuffer& other)
+            : do_destroy_{other.do_destroy_}, do_copy_{other.do_copy_}, object_size_{other.object_size_}
+        {
+            _copy(other);
+        }
+
+        SmallBuffer& operator=(SmallBuffer&& other) noexcept
+        {
+            if (this == std::addressof(other))
+                return *this;
+
+            _destroy();
+
+            do_destroy_ = other.do_destroy_;
+            do_copy_ = other.do_copy_;
+            object_size_ = other.object_size_;
+
+            _move(std::move(other));
+
+            return *this;
+        }
+
+        SmallBuffer& operator=(const SmallBuffer& other) noexcept
+        {
+            if (this == std::addressof(other))
+                return *this;
+
+            _destroy();
+
+            do_destroy_ = other.do_destroy_;
+            do_copy_ = other.do_copy_;
+            object_size_ = other.object_size_;
+
+            _copy(other);
+
+            return *this;
+        }
+
+        template <typename T>
+        requires std::is_copy_constructible_v<T> T& emplace(T&& value)
+        {
+            object_size_ = sizeof(T);
             _destroy();
 
             if constexpr (sizeof(T) <= BufferSize) {
                 ptr_ = &buffer_;
             } else {
-                ptr_ = operator new(sizeof(T));
+                ptr_ = operator new(object_size_);
             }
 
             ::new (ptr_) T{std::forward<T>(value)};
             do_destroy_ = details::SmallBufferImpl<T>::do_destroy;
+            do_copy_ = details::SmallBufferImpl<T>::do_copy;
 
             return unsafe_get_value<T>();
         }
 
         template <typename T>
-        T& unsafe_get_value() noexcept
+        [[nodiscard]] T& unsafe_get_value() noexcept
         {
             RAYCHEL_ASSERT(ptr_ != nullptr);
             return *std::launder(reinterpret_cast<T*>(ptr_));
         }
 
         template <typename T>
-        const T& unsafe_get_value() const noexcept
+        [[nodiscard]] const T& unsafe_get_value() const noexcept
         {
             RAYCHEL_ASSERT(ptr_ != nullptr);
             return *std::launder(reinterpret_cast<const T*>(ptr_));
+        }
+
+        [[nodiscard]] void* unsafe_get_ptr() noexcept
+        {
+            return ptr_;
+        }
+
+        [[nodiscard]] const void* unsafe_get_ptr() const noexcept
+        {
+            return ptr_;
         }
 
         [[nodiscard]] bool is_heap() const noexcept
@@ -116,8 +181,34 @@ namespace Raychel {
             do_destroy_(ptr_, is_heap());
         }
 
+        void _copy(const SmallBuffer& other) noexcept
+        {
+            if (!other.is_heap()) {
+                ptr_ = &buffer_;
+            } else {
+                ptr_ = operator new(object_size_);
+            }
+
+            do_copy_(other.ptr_, ptr_);
+        }
+
+        void _move(SmallBuffer&& other) noexcept
+        {
+            if (!other.is_heap()) {
+                ptr_ = &buffer_;
+                std::memcpy(ptr_, other.ptr_, object_size_);
+                std::memset(other.ptr_, 0, object_size_);
+            } else {
+                ptr_ = other.ptr_;
+            }
+
+            other.ptr_ = nullptr;
+        }
+
         void* ptr_{};
-        DestroyingFunction do_destroy_{};
+        DestroyFunction do_destroy_{};
+        CopyFunction do_copy_{};
+        std::size_t object_size_{};
         std::aligned_storage_t<BufferSize> buffer_{};
     };
 } // namespace Raychel
--- test/SmallBuffer.test.cpp
+++ test/SmallBuffer.test.cpp
@@ -2,38 +2,68 @@
 
 #include <catch2/catch.hpp>
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 
 using Buffer = Raychel::SmallBuffer<16>;
 
 struct SmallStruct
 {
-    std::size_t value{};
-
-    ~SmallStruct() noexcept
-    {
-        std::cout << "SmallStruct destroyed!";
-    }
+    std::uint32_t value{};
 };
 
 struct BigStruct
 {
-    std::size_t data[16]{};
-
-    ~BigStruct() noexcept
-    {
-        std::cout << "BigStruct destroyed!";
-    }
+    std::uint32_t data[16]{};
 };
 
-TEST_CASE("SmallBuffer")
+TEST_CASE("SmallBuffer: construction")
 {
     Buffer buffer{SmallStruct{.value = 12}};
-    REQUIRE(buffer.unsafe_get_value<SmallStruct>().value == 12U);
+    REQUIRE(buffer.unsafe_get_value<SmallStruct>().value == 12);
     REQUIRE_FALSE(buffer.is_heap());
 
     buffer.emplace(BigStruct{});
 
     REQUIRE(buffer.is_heap());
-    REQUIRE(buffer.unsafe_get_value<BigStruct>().data[0] == 0U);
+    REQUIRE(buffer.unsafe_get_value<BigStruct>().data[0] == 0);
+}
+
+TEST_CASE("SmallBuffer: copy")
+{
+    Buffer buffer{SmallStruct{.value = 13}};
+    REQUIRE(buffer.unsafe_get_value<SmallStruct>().value == 13);
+    REQUIRE_FALSE(buffer.is_heap());
+
+    Buffer copy{buffer};
+    REQUIRE(copy.unsafe_get_value<SmallStruct>().value == 13);
+    REQUIRE_FALSE(copy.is_heap());
+
+    const auto temp = Buffer{BigStruct{}};
+
+    copy = temp;
+
+    REQUIRE(copy.unsafe_get_value<BigStruct>().data[0] == 0);
+    REQUIRE(copy.is_heap());
+
+    REQUIRE(temp.unsafe_get_value<BigStruct>().data[0] == 0);
+    REQUIRE(temp.is_heap());
+}
+
+TEST_CASE("SmallBuffer: move")
+{
+    Buffer buffer{SmallStruct{.value = 14}};
+    REQUIRE(buffer.unsafe_get_value<SmallStruct>().value == 14);
+    REQUIRE_FALSE(buffer.is_heap());
+
+    Buffer move{std::move(buffer)};
+    REQUIRE(move.unsafe_get_value<SmallStruct>().value == 14);
+    REQUIRE_FALSE(move.is_heap());
+
+    REQUIRE(buffer.unsafe_get_ptr() == nullptr);
+
+    move = Buffer{BigStruct{}};
+
+    REQUIRE(move.unsafe_get_value<BigStruct>().data[0] == 0);
+    REQUIRE(move.is_heap());
 }
