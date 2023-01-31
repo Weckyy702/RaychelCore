aab382ccbea92978ea0e3a53217c6e58bc3e3426
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2023-01-31 23:17:39 +0100

    WIP! OctTree: continue implementing Raychel::OctTree
    The new Tree design stores the items sequentially in a vector and only
    stores indecies in the nodes, hopefully giving better cache efficiency
    and lowering complexity.

--- include/RaychelCore/OctTree.h
+++ include/RaychelCore/OctTree.h
@@ -28,245 +28,345 @@
 #ifndef RAYCHELCORE_OCTTREE_H
 #define RAYCHELCORE_OCTTREE_H
 
-#include <array>
+#include <algorithm>
 #include <cassert>
 #include <concepts>
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <memory>
-#include <optional>
+#include <numeric>
 #include <type_traits>
 #include <variant>
 #include <vector>
 
 namespace Raychel {
+    // clang-format off
+    template <typename T>
+    concept Coordinate = requires(T t) {
+        requires requires {
+            { t.x } -> std::totally_ordered;
+            { t.y } -> std::totally_ordered;
+            { t.z } -> std::totally_ordered;
+        } || requires {
+            { t.x() } -> std::totally_ordered;
+            { t.y() } -> std::totally_ordered;
+            { t.z() } -> std::totally_ordered;
+        };
+        //TODO: T must be constructible from its three coordinates
+   };
+    // clang-format on
+
+    template <Coordinate Coordinate>
+    struct BoundingBox
+    {
+        Coordinate bottom_front_left, top_back_right;
+    };
 
     namespace details {
-
-        // clang-format off
-
-        template<typename T>
-        concept InvocableCoordinate = requires(T t) {
-            { t.x() } -> ::std::totally_ordered;
-            { t.y() } -> ::std::totally_ordered;
-            { t.z() } -> ::std::totally_ordered;
-        };
-
-        template<typename T>
-        concept MemberCoordinate = requires(T t) {
-            { t.x } -> ::std::totally_ordered;
-            { t.y } -> ::std::totally_ordered;
-            { t.z } -> ::std::totally_ordered;
-        };
-
-        template<typename T>
-        concept Coordinate = InvocableCoordinate<T> || MemberCoordinate<T>;
-        // clang-format on
-
-        template <std::totally_ordered T>
-        constexpr bool in_range(T value, T min, T max)
-        {
-            return (value >= min) && (value <= max);
-        }
-
-        template <typename Object, auto Member>
-        constexpr auto get(const Object& obj)
+        template <typename Obj, auto Member>
+            requires(std::is_member_pointer_v<decltype(Member)>)
+        [[nodiscard]] constexpr auto get_member(const Obj& obj)
         {
             return std::invoke(Member, obj);
         }
 
-        template <Coordinate Object>
-        constexpr auto get_x = get<Object, &Object::x>;
+        template <Coordinate Coord>
+        constexpr auto get_x = get_member<Coord, &Coord::x>;
 
-        template <Coordinate Object>
-        constexpr auto get_y = get<Object, &Object::y>;
+        template <Coordinate Coord>
+        constexpr auto get_y = get_member<Coord, &Coord::y>;
 
-        template <Coordinate Object>
-        constexpr auto get_z = get<Object, &Object::z>;
+        template <Coordinate Coord>
+        constexpr auto get_z = get_member<Coord, &Coord::z>;
 
-        template <Coordinate C, Coordinate BB>
-        constexpr bool contained_in(const C& coord, const BB& top_left, const BB& bottom_right)
+        template <Coordinate Coord>
+        constexpr Coord midpoint(const BoundingBox<Coord> box)
         {
-            return in_range(get_x<C>(coord), get_x<BB>(top_left), get_x<BB>(bottom_right)) &&
-                   in_range(get_y<C>(coord), get_y<BB>(top_left), get_y<BB>(bottom_right)) &&
-                   in_range(get_z<C>(coord), get_z<BB>(top_left), get_z<BB>(bottom_right));
+            return Coord{
+                std::midpoint(get_x<Coord>(box.bottom_front_left), get_x<Coord>(box.top_back_right)),
+                std::midpoint(get_y<Coord>(box.bottom_front_left), get_y<Coord>(box.top_back_right)),
+                std::midpoint(get_z<Coord>(box.bottom_front_left), get_z<Coord>(box.top_back_right)),
+            };
         }
 
-        struct ContainedIn
+        template <std::totally_ordered T>
+        constexpr bool in_range(const T& value, const T& min, const T& max)
         {
-            template <Coordinate T>
-            constexpr bool operator()(const T& coord, const T& top_left, const T& bottom_right)
+            return (value >= min) && (value <= max);
+        }
+
+        template <Coordinate Coord>
+        [[nodiscard]] constexpr bool contained_in(const Coord& coordinate, const BoundingBox<Coord>& bounding_box)
+        {
+            return in_range(
+                       get_x<Coord>(coordinate),
+                       get_x<Coord>(bounding_box.bottom_front_left),
+                       get_x<Coord>(bounding_box.top_back_right)) &&
+                   in_range(
+                       get_y<Coord>(coordinate),
+                       get_y<Coord>(bounding_box.bottom_front_left),
+                       get_y<Coord>(bounding_box.top_back_right)) &&
+                   in_range(
+                       get_z<Coord>(coordinate),
+                       get_z<Coord>(bounding_box.bottom_front_left),
+                       get_z<Coord>(bounding_box.top_back_right));
+        }
+
+        template <std::size_t Index, Coordinate Coord>
+        [[nodiscard]] constexpr BoundingBox<Coord>
+        bounding_box_for(const BoundingBox<Coord>& bounding_box, const Coord& midpoint) noexcept
+        {
+            //TODO: implement
+            assert(false);
+
+            BoundingBox<Coord> res{Coord{0, 0, 0}, Coord{0, 0, 0}};
+
+            if (Index & 1U) {
+            }
+
+            if (Index & 2U) {
+            }
+
+            if (Index & 4U) {
+            }
+
+            return res;
+        }
+
+        template <Coordinate Coord>
+        [[nodiscard]] std::array<BoundingBox<Coord>, 8>
+        subdivide_bounding_box(const BoundingBox<Coord>& bounding_box, const Coord& midpoint) noexcept
+        {
+            assert(false);
+            return {};
+        }
+
+        struct T2Coord
+        {
+            template <std::copyable T>
+            [[nodiscard]] constexpr T operator()(const T& obj) noexcept
             {
-                return contained_in(coord, top_left, bottom_right);
+                return obj;
             }
         };
 
-        template <
-            typename T, std::size_t BucketCapacity, Coordinate Coord,
-            std::invocable<const T&, const Coord&, const Coord&> ContainedIn>
+        template <std::size_t BucketSize, Coordinate Coordinate>
         class OctNode
         {
+            using BoundingBox = BoundingBox<Coordinate>;
 
-            class Bucket
+            class IndeciesContainer
             {
             public:
                 [[nodiscard]] constexpr bool is_full() const noexcept
                 {
-                    return next_index_ == BucketCapacity;
+                    return next_slot_ == BucketSize;
                 }
 
-                constexpr void insert(T value) noexcept(std::is_nothrow_move_constructible_v<T>)
+                constexpr void insert(std::size_t index_in_tree, const Coordinate& where) noexcept
                 {
-                    if (is_full())
-                        return;
+                    indecies_[next_slot_] = index_in_tree;
+                    new (_locations() + next_slot_) Coordinate{where};
 
-                    new (_current()) T{std::move(value)};
-                    ++next_index_;
+                    ++next_slot_;
+                }
+
+                [[nodiscard]] constexpr Coordinate coord_at(std::size_t index) const noexcept
+                {
+                    return _locations()[index];
+                }
+
+                [[nodiscard]] constexpr std::size_t index_at(std::size_t index) const noexcept
+                {
+                    return indecies_[index];
                 }
 
             private:
-                constexpr T* _current() noexcept
+                [[nodiscard]] constexpr const Coordinate* _locations() const noexcept
                 {
-                    return _items() + next_index_;
+                    return reinterpret_cast<const Coordinate*>(&index_data);
                 }
 
-                constexpr T* _items()
+                [[nodiscard]] constexpr Coordinate* _locations() noexcept
                 {
-                    return reinterpret_cast<T*>(&storage_);
+                    return reinterpret_cast<Coordinate*>(&index_data);
                 }
 
-                std::size_t next_index_{};
-                std::aligned_storage_t<sizeof(T) * BucketCapacity, alignof(T)> storage_{};
+                std::array<std::size_t, BucketSize> indecies_;
+                std::aligned_storage_t<sizeof(Coordinate) * BucketSize> index_data{};
+                std::size_t next_slot_{};
             };
 
             class ChildrenContainer
             {
-                struct Child
-                {
-                    std::size_t index_in_parent;
-                    OctNode child;
-                };
-
-                using Children = Child[];
+                using Children = OctNode[];
 
             public:
-                explicit ChildrenContainer() : children_{std::make_unique<Children>(8)}
-                {}
+                constexpr explicit ChildrenContainer(std::array<BoundingBox, 8> bounding_boxes)
+                    : children_{reinterpret_cast<OctNode*>(::operator new(sizeof(OctNode) * 8U))}
+                {
+                    for (std::size_t i{}; i != 8; ++i) {
+                        new (children_.get() + i) OctNode{bounding_boxes[i]};
+                    }
+                }
+
+                [[nodiscard]] constexpr OctNode& operator[](std::size_t index) noexcept
+                {
+                    return children_[index];
+                }
 
             private:
                 std::unique_ptr<Children> children_;
             };
 
         public:
-            OctNode(Coord top_back_left, Coord bottom_front_right) noexcept(std::is_nothrow_move_constructible_v<Coord>)
-                : elements_or_children_{Bucket{}},
-                  top_left_corner_{std::move(top_back_left)},
-                  bottom_right_corner_{std::move(bottom_front_right)}
+            explicit constexpr OctNode(BoundingBox bounding_box)
+                : bounding_box_{std::move(bounding_box)}, midpoint_{midpoint(bounding_box_)}
             {}
+
+            [[nodiscard]] constexpr BoundingBox bounding_box() const noexcept
+            {
+                return bounding_box_;
+            }
 
             [[nodiscard]] constexpr bool has_children() const noexcept
             {
-                return elements_or_children_.index() == 1U;
+                return indecies_or_children_.index() == 1U;
             }
 
-            [[nodiscard]] constexpr bool insert(T value) noexcept(std::is_nothrow_move_constructible_v<T>)
+            constexpr void insert(std::size_t index_in_tree, const Coordinate& where) noexcept
             {
-                if (!_contained_in(value, top_left_corner_, bottom_right_corner_)) {
-                    return false;
-                }
-
+                (void)where;
                 if (has_children()) {
-                    _insert_into_children(std::move(value));
-                } else {
-                    _insert_into_bucket(std::move(value));
-                }
-
-                return true;
-            }
-
-        private:
-            constexpr void _insert_into_bucket(T value) noexcept(std::is_nothrow_move_constructible_v<T>)
-            {
-                Bucket& bucket = std::get<Bucket>(elements_or_children_);
-
-                if (bucket.is_full()) {
-                    //TODO
-                    assert(false);
+                    _insert_into_children(index_in_tree, where);
                     return;
                 }
 
-                bucket.insert(std::move(value));
+                IndeciesContainer& bucket = std::get<IndeciesContainer>(indecies_or_children_);
+
+                if (bucket.is_full()) {
+                    //Subdivide and insert into children
+                    _subdivide();
+                    _insert_into_children(index_in_tree, where);
+                    return;
+                }
+
+                bucket.insert(index_in_tree, where);
             }
 
-            constexpr void _insert_into_children(T value) noexcept(std::is_nothrow_move_constructible_v<T>)
+        private:
+            [[nodiscard]] constexpr std::size_t _child_index_for(Coordinate where)
             {
-                //TODO
-                (void)value;
-                assert(false);
+                //!!BITMAGIC!!
+                // returns binary |00000000|...|00000abc|
+                // where a = x-axis; b = y-axis; c = z-axis
+
+                std::size_t index{};
+                if (get_x<Coordinate>(where) >= get_x<Coordinate>(midpoint_)) {
+                    index |= 1U;
+                }
+                if (get_y<Coordinate>(where) >= get_y<Coordinate>(midpoint_)) {
+                    index |= 2U;
+                }
+                if (get_z<Coordinate>(where) >= get_z<Coordinate>(midpoint_)) {
+                    index |= 4U;
+                }
+
+                return index;
+            }
+
+            constexpr void _insert_into_children(std::size_t index_in_tree, const Coordinate& where) noexcept
+            {
+                const auto child_index = _child_index_for(where);
+                auto& children = std::get<ChildrenContainer>(indecies_or_children_);
+
+                children[child_index].insert(index_in_tree, where);
             }
 
             constexpr void _subdivide() noexcept
             {
-                //Noop if we are already divided (shouldn't happen?)
-                if (has_children()) {
-                    return;
-                }
+                //Save current items
+                auto items = std::get<IndeciesContainer>(std::move(indecies_or_children_));
 
                 //Create children
-                // Divide bounds into eight smaller subregions
-                // Create new OctNode for each subregion
+                // Create 8 children, each with its own subdivision of the bounding box and
+                [[maybe_unused]] auto& children =
+                    indecies_or_children_.template emplace<ChildrenContainer>(subdivide_bounding_box(bounding_box_, midpoint_));
 
-                //Sort items into children
-                // For every item
-                //  Calculate which child it belongs into
-                //  Put it into that node
+                // Put the items into the children using their coordinates
+                // TODO: try doing this in one step while initializing the children
+                for (std::size_t i{}; i != BucketSize; ++i) {
+                    const auto where = items.coord_at(i);
+                    const auto index = _child_index_for(where);
 
-                //Disgard empty children? (They use quite a bit of memory)
+                    children[index].insert(items.index_at(i), where);
+                }
             }
 
-            constexpr auto _elements() noexcept
-            {
-                return std::get<0>(std::move(elements_or_children_));
-            }
+            std::variant<IndeciesContainer, ChildrenContainer> indecies_or_children_{};
 
-            std::variant<Bucket, ChildrenContainer> elements_or_children_;
-            Coord top_left_corner_;
-            Coord bottom_right_corner_;
-
-            ContainedIn _contained_in{};
+            BoundingBox bounding_box_;
+            Coordinate midpoint_;
         };
-    }; // namespace details
+    } // namespace details
 
-    template <typename T, std::size_t BucketCapacity, typename Coordinates, typename ContainedIn = details::ContainedIn>
+    template <
+        typename T, std::size_t BucketSize = 10, Coordinate Coordinate = T, std::invocable<const T&> T2Coord = details::T2Coord>
+        requires(std::is_invocable_r_v<Coordinate, T2Coord, const T&>)
     class OctTree
     {
-        using Node = details::OctNode<T, BucketCapacity, Coordinates, ContainedIn>;
+        using Node = details::OctNode<BucketSize, Coordinate>;
+        using BoundingBox = BoundingBox<Coordinate>;
 
     public:
-        OctTree(Coordinates top_back_left, Coordinates bottom_front_right)
-            : root_{std::move(top_back_left), std::move(bottom_front_right)}
+        constexpr explicit OctTree(BoundingBox bounding_box) : root_{bounding_box}
         {}
 
-        [[nodiscard]] constexpr std::size_t size()
+        [[nodiscard]] constexpr std::size_t size() const noexcept
         {
-            return size_;
+            return points_.size();
         }
 
         constexpr bool insert(T value)
         {
-            if (root_.insert(std::move(value))) {
-                ++size_;
-                return true;
-            }
-            return false;
+            const auto where = _t2coord(value);
+
+            if (!details::contained_in(where, root_.bounding_box()))
+                return false;
+
+            points_.push_back(std::move(value));
+            root_.insert(points_.size() - 1, where);
+
+            return true;
+        }
+
+        constexpr auto begin() noexcept
+        {
+            return points_.begin();
+        }
+
+        constexpr auto end() noexcept
+        {
+            return points_.end();
+        }
+
+        constexpr auto cbegin() const noexcept
+        {
+            return points_.cbegin();
+        }
+
+        constexpr auto cend() const noexcept
+        {
+            return points_.cend();
         }
 
     private:
         Node root_;
-        std::size_t size_{};
-
-        ContainedIn _contained_in{};
+        std::vector<T> points_{};
+        T2Coord _t2coord{};
     };
+
 } //namespace Raychel
 
 #endif //!RAYCHELCORE_OCTTREE_H
--- test/OctTree.test.cpp
+++ test/OctTree.test.cpp
@@ -13,18 +13,18 @@ struct vec3
     double x, y, z;
 };
 
-using OctTree = Raychel::OctTree<vec3, 5, vec3>;
+using OctTree = Raychel::OctTree<vec3, 5>;
 
 TEST_CASE("OctTree: creating OctTrees")
 {
-    OctTree tree{vec3{-100, -100, -100}, vec3{100, 100, 100}};
+    OctTree tree{{vec3{-100, -100, -100}, vec3{100, 100, 100}}};
 
     REQUIRE(tree.size() == 0);
 }
 
 TEST_CASE("OctTree: Inserting N elements into the tree")
 {
-    OctTree tree{vec3{-100, -100, -100}, vec3{100, 100, 100}};
+    OctTree tree{{vec3{-100, -100, -100}, vec3{100, 100, 100}}};
 
     for (std::size_t _i{}; _i != 5; ++_i) {
         const auto i = static_cast<double>(_i);
@@ -35,4 +35,13 @@ TEST_CASE("OctTree: Inserting N elements into the tree")
 }
 
 TEST_CASE("OctTree: Subdivison")
-{}
+{
+    OctTree tree{{vec3{-100, -100, -100}, vec3{100, 100, 100}}};
+
+    for (std::size_t _i{}; _i != 10; ++_i) {
+        const auto i = static_cast<double>(_i);
+
+        REQUIRE(tree.insert(vec3{i, i, i}));
+        REQUIRE(tree.size() == _i + 1);
+    }
+}
